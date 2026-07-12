b23a79cac5ecea5e79a9a3dfdc5d2cb6fd015400
lapapitacalientee-png <lapapitacalientee@gmail.com>
2026-07-12 00:30:12 -0400

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,1 +1,13 @@
+module.exports = {
+    name: "ping",
 
+    async execute(message, client) {
+
+        const msg = await message.reply("🏓 Calculating...");
+
+        await msg.edit(
+            `🏓 Pong! Ping: ${client.ws.ping}ms`
+        );
+
+    }
+};
