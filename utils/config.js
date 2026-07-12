94a65b6f9a610af3d6ff1b2d91b1b1535d82f483
lapapitacalientee-png <lapapitacalientee@gmail.com>
2026-07-12 00:29:32 -0400

    Update config.js

--- utils/config.js
+++ utils/config.js
@@ -1,1 +1,19 @@
+const fs = require("fs");
 
+function getConfig() {
+    return JSON.parse(
+        fs.readFileSync("./config.json", "utf8")
+    );
+}
+
+function saveConfig(config) {
+    fs.writeFileSync(
+        "./config.json",
+        JSON.stringify(config, null, 2)
+    );
+}
+
+module.exports = {
+    getConfig,
+    saveConfig
+};
