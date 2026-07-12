ec69e689a6befb0e6e3cac7257e50ae846f1d520
lapapitacalientee-png <lapapitacalientee@gmail.com>
2026-07-12 00:30:38 -0400

    Update setuplog.js

--- commands/setuplog.js
+++ commands/setuplog.js
@@ -1,1 +1,26 @@
+const { getConfig, saveConfig } = require("../utils/config");
 
+module.exports = {
+    name: "setuplog",
+
+    async execute(message) {
+
+        const channel = message.mentions.channels.first();
+
+        if (!channel) {
+            return message.reply(
+                "❌ Mention a channel. Example: `;setuplog #logs`"
+            );
+        }
+
+        const config = getConfig();
+
+        config.logChannel = channel.id;
+
+        saveConfig(config);
+
+        message.reply(
+            `✅ Log channel set: ${channel}`
+        );
+    }
+};
