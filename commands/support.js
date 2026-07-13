0007b6799d3d1939b3485d4d6e42f53d744ba9a1
lapapitacalientee-png <lapapitacalientee@gmail.com>
2026-07-13 17:53:27 -0400

    Update support.js

--- commands/support.js
+++ commands/support.js
@@ -32,7 +32,7 @@ module.exports = {
         try {
 
             const response = await fetch(
-                `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${process.env.GEMINI_API_KEY}`,
+                `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash-lite:generateContent?key=${process.env.GEMINI_API_KEY}`,
                 {
                     method: "POST",
                     headers: {
@@ -119,4 +119,4 @@ module.exports = {
     }
 
 };
-        
+                    
