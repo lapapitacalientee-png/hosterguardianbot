2b07be688b9f2115315b92e373dec86657483e48
lapapitacalientee-png <lapapitacalientee@gmail.com>
2026-07-15 21:43:22 -0400

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -9,17 +9,7 @@ module.exports = {
         const commands = [...client.commands.values()]
             .sort((a, b) => a.name.localeCompare(b.name));
 
-        const longestName = Math.max(
-            ...commands.map(cmd => cmd.name.length)
-        );
-
-        const lines = commands.map(cmd => {
-
-            const padding = " ".repeat(longestName - cmd.name.length + 2);
-
-            return `  ${cmd.name}${padding}${cmd.description || ""}`;
-
-        });
+        const lines = commands.map(cmd => `  ${cmd.name}`);
 
         const content =
             "```\nNo Category:\n" +
